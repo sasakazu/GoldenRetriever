41c99ce8ebf2176707ac97e6bd1ac0d0935202ab
kazuyasasakura <kazuya.sasakura@gmial.com>
2019-06-14 17:11:36 +0900

    realtime database start...

---
+++ GoldenRetriever/EveryOne/addPhoto.swift
@@ -0,0 +1,31 @@
+//
+//  addPhoto.swift
+//  GoldenRetriever
+//
+//  Created by 笹倉一也 on 2019/06/14.
+//  Copyright © 2019 笹倉一也. All rights reserved.
+//
+
+import UIKit
+
+class addPhoto: UIViewController {
+    
+    @IBOutlet weak var nameTF: UITextField!
+    @IBOutlet weak var friend1TF: UITextField!
+    @IBOutlet weak var firiendTF2: UITextField!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func saveBtn(_ sender: Any) {
+    }
+    
+    
+    
+}
